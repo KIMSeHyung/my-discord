ea409777175e57a12b4ecab658b83be86449000e
KIMSeHyung <tpgud33@gmail.com>
2025-04-06 19:23:57 +0900

    logger middleware  추가가

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger();
   getHello(): string {
+    this.logger.debug('AppService!');
     return 'Hello World!';
   }
 }
