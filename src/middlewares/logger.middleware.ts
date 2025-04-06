f9f9a90f59fddeb98b072581113aec549e8706d3
KIMSeHyung <tpgud33@gmail.com>
2025-04-06 19:39:08 +0900

    response 롤백백

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,11 +1,11 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
-import { Request, NextFunction } from 'express';
+import { Request, NextFunction, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP'); // HTTP(context)의 역할 -> HTTP 관련된 요청에서만 logger가 실행 됨 , express의 debug 라이브러리와 같은 역할
 
-  use(request: Request, next: NextFunction): void {
+  use(request: Request, response: Response, next: NextFunction): void {
     const { ip, method, originalUrl, body, query, params } = request;
     const userAgent = request.get('user-agent') || ''; // header에서 가져옴
 
